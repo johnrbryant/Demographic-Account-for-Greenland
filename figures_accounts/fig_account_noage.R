bdaf114f66a3ae152c788584256b7b0de0b57c6e
johnrbryant <john.r.bryant@gmail.com>
2021-10-17 16:50:15 +1300

    added figures_accounts

---
+++ figures_accounts/fig_account_noage.R
@@ -0,0 +1,57 @@
+
+
+library(ggplot2)
+
+
+data_popn_box <- data.frame(x = c(0, 2, 2, 0, 1),
+                            xend = c(2, 2, 0, 0, 1),
+                            y = c(0, 0, 1, 1, 0),
+                            yend = c(0, 1, 1, 0, 1))
+data_popn_label <- data.frame(x = c(1, 0.5, 1.5, 0.5, 1.5),
+                              y = c(1.2, 0.5, 0.5, -0.3, -0.3),
+                              label = c("Population", 6, 7, "italic(t)", "italic(t)+1"))
+data_births_box <- data.frame(x = c(2.5, 3.5, 3.5, 2.5),
+                              xend = c(3.5, 3.5, 2.5, 2.5),
+                              y = c(0, 0, 1, 1),
+                              yend = c(0, 1, 1, 0))
+data_births_label <- data.frame(x = c(3, 3, 3),
+                                y = c(1.2, 0.5, -0.3),
+                                label = c("Births", 3, 'group("[",list(italic(t),italic(t+1)),")")'))
+data_deaths_box <- data.frame(x = c(4, 5, 5, 4),
+                              xend = c(5, 5, 4, 4),
+                              y = c(0, 0, 1, 1),
+                              yend = c(0, 1, 1, 0))
+data_deaths_label <- data.frame(x = c(4.5, 4.5, 4.5),
+                                y = c(1.2, 0.5, -0.3),
+                                label = c("Deaths", 2, 'group("[",list(italic(t),italic(t+1)),")")'))
+p <- ggplot(data_popn_box, aes(x = x, y = y)) +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank()) +
+    coord_cartesian(expand = FALSE,
+                    clip = "off") +
+    ## population
+    geom_segment(aes(x = x, y = y, xend = xend, yend = yend),
+                 data = data_popn_box) +
+    geom_text(aes(label = label),
+              data = data_popn_label,
+              parse = TRUE) +
+    ## births
+    geom_segment(aes(x = x, y = y, xend = xend, yend = yend),
+                 data = data_births_box) +
+    geom_text(aes(label = label),
+              data = data_births_label,
+              parse = TRUE) +
+    ## deaths
+    geom_segment(aes(x = x, y = y, xend = xend, yend = yend),
+                 data = data_deaths_box) +
+    geom_text(aes(label = label),
+              data = data_deaths_label,
+              parse = TRUE) +
+    ## other
+    xlab("") +
+    ylab("")
+p
