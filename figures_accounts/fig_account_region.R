b2142ca53a670112baa4164c21e503ad989547de
John Bryant <john.r.bryant@gmail.com>
2021-10-19 20:25:15 +1300

    modified figures and added lit

--- figures_accounts/fig_account_region.R
+++ figures_accounts/fig_account_region.R
@@ -7,19 +7,19 @@ data_popn_box <- data.frame(x =    c(0, 2, 2, 0, 1, 0),
                             y =    c(0, 0, 2, 2, 0, 1),
                             yend = c(0, 2, 2, 0, 2, 1))
 data_popn_label <- data.frame(x = c(1, 0.5, 1.5, 0.5, 1.5, 0.5, 1.5),
-                              y = c(2.2, 0.5, 0.5, 1.5, 1.5, -0.15, -0.15),
+                              y = c(2.2, 0.5, 0.5, 1.5, 1.5, -0.4, -0.4),
                               label = c("Population", 10, 14, 15, 11, "italic(t)", "italic(t)+1"))
 data_mig_box <- data.frame(x =     c(3, 5, 5, 3, 4, 3),
                             xend = c(5, 5, 3, 3, 4, 5),
                             y =    c(0, 0, 2, 2, 0, 1),
                             yend = c(0, 2, 2, 0, 2, 1))
-data_mig_label <- data.frame(x =   c(4, 3.5,  4.5, 3.5, 4.5,  4, 2.85, 2.85),
-                             y =  c(2.4, 0.5, 1.5, 2.15, 2.15, -0.15, 0.5, 1.5),
+data_mig_label <- data.frame(x =   c(4, 3.5,  4.5, 3.5, 4.5,  4, 2.8, 2.8),
+                             y =  c(2.5, 0.5, 1.5, 2.2, 2.2, -0.4, 0.5, 1.5),
                              label = c("Migration", 1, 5, "A", "B",
                                        'group("[",list(italic(t),italic(t+1)),")")',
                                        "B", "A"))
 
-data_region_label <- data.frame(x = rep(-0.15, 2),
+data_region_label <- data.frame(x = rep(-0.2, 2),
                                 y = c(0.5, 1.5),
                                 label = c("B", "A"))
                                        
@@ -48,12 +48,12 @@ p <- ggplot(data_popn_box, aes(x = x, y = y)) +
     geom_text(aes(label = label),
               data = data_region_label) +
     ## other
-    xlab("Time") +
-    ylab("Region")
+    xlab("") +
+    ylab("")
 
 graphics.off()
 pdf(file = "figures_accounts/fig_account_region.pdf",
-    width = 4,
-    height = 2.1)
+    width = 3.2,
+    height = 1.8)
 plot(p)
 dev.off()
--- figures_accounts/fig_account_withage2.R
+++ figures_accounts/fig_account_withage2.R
@@ -9,20 +9,20 @@ data_popn_box <- data.frame(x =    c(0, 2, 2, 0, 1, 0, 0),
 data_popn_label <- data.frame(x = c(1, 0.5, 1.5, 0.5, 1.5, 0.5, 1.5, 0.5, 1.5),
                               y = c(3.2, 0.5, 0.5, 1.5, 1.5, 2.5, 2.5, -0.4, -0.4),
                               label = c("Population", 7, 6, 4, 5, 5, 6, "italic(t)", "italic(t)+1"))
-data_births_box <- data.frame(x =    c(2.5, 3.5, 3.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5),
-                              xend = c(3.5, 3.5, 2.5, 2.5, 3.5, 3.5, 3.5, 3.5, 3.5),
-                              y =    c(0, 0, 3, 3, 1, 2, 0, 1, 2),
-                              yend = c(0, 3,   3,     0, 1,   2, 1, 2, 3))
-data_births_label <- data.frame(x =  c(3,   2.7, 3.3, 2.7, 3.3, 2.7, 3.3, 3),
-                                y =  c(3.2, 0.7, 0.3, 1.7, 1.3, 2.7, 2.3, -0.4),
-                                label = c("Births", 0, 0, 3, 5, 0, 0, 'group("[",list(italic(t),italic(t+1)),")")'))
-data_deaths_box <- data.frame(x =    c(4, 5, 5, 4, 4, 4, 4, 4, 4),
-                              xend = c(5, 5, 4, 4, 5, 5, 5, 5, 5),
-                              y =    c(0, 0, 3, 3, 1, 2, 0, 1, 2),
-                              yend = c(0, 3, 3, 0, 1, 2, 1, 2, 3))
-data_deaths_label <- data.frame(x = c(4.5, 4.2, 4.7, 4.2, 4.7, 4.2, 4.7, 4.5),
-                                y = c(3.2, 0.7, 0.3, 1.7, 1.3, 2.7, 2.3, -0.4),
-                                label = c("Deaths", 1, 2, 0, 1, 2, 1, 'group("[",list(italic(t),italic(t+1)),")")'))
+data_births_box <- data.frame(x =    c(2.5, 3.5, 3.5, 2.5, 2.5, 2.5),
+                              xend = c(3.5, 3.5, 2.5, 2.5, 3.5, 3.5),
+                              y =    c(0, 0, 3, 3, 1, 2),
+                              yend = c(0, 3,   3,     0, 1,   2))
+data_births_label <- data.frame(x =  c(3,   3, 3, 3, 3),
+                                y =  c(3.2, 0.5, 1.5, 2.5, -0.4),
+                                label = c("Births", 0, 8, 0, 'group("[",list(italic(t),italic(t+1)),")")'))
+data_deaths_box <- data.frame(x =    c(4, 5, 5, 4, 4, 4),
+                              xend = c(5, 5, 4, 4, 5, 5),
+                              y =    c(0, 0, 3, 3, 1, 2),
+                              yend = c(0, 3, 3, 0, 1, 2))
+data_deaths_label <- data.frame(x = c(4.5, 4.5, 4.5, 4.5, 4.5),
+                                y = c(3.2, 0.5, 1.5, 2.5, -0.4),
+                                label = c("Deaths",3, 1, 3, 'group("[",list(italic(t),italic(t+1)),")")'))
 data_age_label <- data.frame(x = rep(-0.6, 3),
                              y = c(0.5, 1.5, 2.5),
                              label = c("Age 0", "Age 1", "Age 2+"))
@@ -62,7 +62,7 @@ p <- ggplot(data_popn_box, aes(x = x, y = y)) +
     ylab("")
 
 graphics.off()
-pdf(file = "figures_accounts/fig_account_withage.pdf",
+pdf(file = "figures_accounts/fig_account_withage2.pdf",
     width = 3,
     height = 2)
 plot(p)
