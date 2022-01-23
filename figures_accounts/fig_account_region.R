573232cd4809de7b9ef38c416bf50dddd355008c
John Bryant <john.r.bryant@gmail.com>
2022-01-23 14:18:13 +0700

    revised figure labels

--- figures_accounts/fig_account_region.R
+++ figures_accounts/fig_account_region.R
@@ -7,14 +7,14 @@ data_popn_box <- data.frame(x =    c(0, 2, 2, 0, 1, 0),
                             y =    c(0, 0, 2, 2, 0, 1),
                             yend = c(0, 2, 2, 0, 2, 1))
 data_popn_label <- data.frame(x = c(1, 0.5, 1.5, 0.5, 1.5, 0.5, 1.5),
-                              y = c(2.2, 0.5, 0.5, 1.5, 1.5, -0.4, -0.4),
+                              y = c(2.3, 0.5, 0.5, 1.5, 1.5, -0.4, -0.4),
                               label = c("Population", 6, 9, 10, 8, "italic(t)", "italic(t)+1"))
 data_births_box <- data.frame(x =    c(3, 4, 4, 3, 3),
                               xend = c(4, 4, 3, 3, 4),
                               y =    c(0, 0, 2, 2, 1),
                               yend = c(0, 2, 2, 0, 1))
 data_births_label <- data.frame(x =  c(3.5, 3.5, 3.5,  3.5, 2.8, 2.8),
-                                y =  c(2.2, 0.5, 1.5, -0.4, 0.5, 1.5),
+                                y =  c(2.35, 0.5, 1.5, -0.4, 0.5, 1.5),
                                 label = c("Births", 5, 3, 
                                           'group("[",list(italic(t),italic(t+1)),")")',
                                           "B", "A"))
@@ -23,7 +23,7 @@ data_deaths_box <- data.frame(x =    c(5, 6, 6, 5, 5),
                               y =    c(0, 0, 2, 2, 1),
                               yend = c(0, 2, 2, 0, 1))
 data_deaths_label <- data.frame(x =  c(5.5, 5.5, 5.5,  5.5, 4.8, 4.8),
-                                y =  c(2.2, 0.5, 1.5, -0.4, 0.5, 1.5),
+                                y =  c(2.35, 0.5, 1.5, -0.4, 0.5, 1.5),
                                 label = c("Deaths", 3, 4, 
                                           'group("[",list(italic(t),italic(t+1)),")")',
                                           "B", "A"))
@@ -32,7 +32,7 @@ data_mig_box <- data.frame(x =     c(3, 5, 5, 3, 4, 3) + 4,
                             y =    c(0, 0, 2, 2, 0, 1),
                             yend = c(0, 2, 2, 0, 2, 1))
 data_mig_label <- data.frame(x =   c(4, 3.5,  4.5, 3.5, 4.5,  4, 2.8, 2.8) + 4,
-                             y =  c(2.5, 0.5, 1.5, 2.2, 2.2, -0.4, 0.5, 1.5),
+                             y =  c(2.6, 0.5, 1.5, 2.2, 2.2, -0.4, 0.5, 1.5),
                              label = c("Moves", 6, 7, "A", "B",
                                        'group("[",list(italic(t),italic(t+1)),")")',
                                        "B", "A"))
